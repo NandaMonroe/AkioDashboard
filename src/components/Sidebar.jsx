f1b33df121d3d6448b05ce8a474d2f225950f06d
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-01 22:39:43 -0300

    Add sidebar menu with Akio logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import { SiSentry } from "react-icons/si";
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { links } from '../data/dummy';
+// import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
+
+  const activeMenu = true;
+
   return (
-    <div>Sidebar</div>
-  )
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link to="/" onClick={() => {}} className="items-center">
+              <SiSentry /> <span>Akio</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Sidebar

d2e0bd2f908c53e79aab3d4e3676e1be265aa04d
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-05 23:13:17 -0300

    Refactor Sidebar component and fix formatting issues

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import { SiSentry } from "react-icons/si";
-import { MdOutlineCancel } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { links } from '../data/dummy';
+import { links } from "../data/dummy";
 // import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-
   const activeMenu = true;
 
   return (
@@ -16,14 +15,47 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={() => {}} className="items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
               <SiSentry /> <span>Akio</span>
             </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => {}}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
           </div>
-        </>
-      )}
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => {}}
+                    className={(( isActive ))}
+                    activeClassName="text-blue-500"
+                  >
+                    {link.icon}
+                    <span>{link.title}</span>
+                  </NavLink>
+                ))}
+              </div>
+              ))}
+          </div>
+        </>)}
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;

bd853bf41e539fddea476114a950211cdeb9912c
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-11 18:28:38 -0300

    Refactor Stacked component with Syncfusion charts

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+import { Header } from '../../components';
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = () => {
-  return (
-    <div>Stacked</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Stacked
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Stacked" title="Budget/Expense Comparison" />
+      <div className="w-full flex flex-auto items-center">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={stackedPrimaryXAxis}
+          primaryYAxis={stackedPrimaryYAxis}
+          width="320px"
+          height="360px"
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject
+            services={[StackingColumnSeries, Category, Legend, Tooltip]}
+          />
+          <SeriesCollectionDirective>
+            {stackedCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Stacked;

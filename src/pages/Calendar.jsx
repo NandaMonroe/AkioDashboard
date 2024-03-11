3543d703225faf0b6f05e6a30f28b23a87a7deaa
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-10 23:02:25 -0300

    Add calendar component with schedule functionality

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
+// eslint-disable-next-line react/destructuring-assignment
+const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
 
 const Calendar = () => {
+
   return (
-    <div>Calendar</div>
-  )
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2024, 2, 10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
+  );
 }
 
 export default Calendar

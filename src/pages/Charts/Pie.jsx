d05864cb6e4ce41e17a5827e56043a468aec0db1
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-11 18:27:36 -0300

    Refactor Pie component to include header and pie chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import { pieChartData } from '../../data/dummy';
+import { Header, Pie as PieChart } from '../../components';
 
 const Pie = () => {
   return (
-    <div>Pie</div>
-  )
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <div className="m-auto">
+        <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
+      </div>
+    </div>
+  );
 }
 
 export default Pie

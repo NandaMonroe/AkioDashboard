36e68afcfc207d49741e8ea16a83ead9cb307c8f
Fernanda Monroe <inandamonroe@gmail.com>
2024-03-11 18:30:55 -0300

    Update Footer component with copyright information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+        © 2024 All rights reserved by nandamonroe.vercel.app
+      </p>
+    </div>
+  );
 }
 
 export default Footer
